7db466628be6539fc1f4aa537b1dda701b89a1e0
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2019-11-22 22:38:26 +0700

    Revert "Revert "Insert and Update produk""
    This reverts commit 14d2bddfe3070f6eb38b2697149438dd5e29232a.

--- application/controllers/c_admin.php
+++ application/controllers/c_admin.php
@@ -11,38 +11,90 @@ class C_admin extends CI_Controller {
                 redirect(base_url().'c_login?alert=belum_login');
                 }
     }
-//untuk menampilkan ke halaman dashboard
+
+    // ======================================== VIEW ======================================== \\
+
+    //untuk menampilkan ke halaman dashboard
     public function index(){
         $this->load->view('admin/v_header');
         $this->load->view('admin/v_index');
         $this->load->view('admin/v_footer');
 
     }
-//untuk menampilkan ke halaman produk 
+    //untuk menampilkan ke halaman produk 
     function produk(){
         $data['catalog'] = $this->m_data->get_data('catalog_list')->result();
         $this->load->view('admin/v_header');
         $this->load->view('admin/v_produk', $data);
         $this->load->view('admin/v_footer');
     }
-//untuk menampilkan ke halaman produk 
+    // untuk edit data produk
+    function produk_edit($id){
+        $where = array('id'=> $id);
+        $data['data_catalog'] = $this->m_data->edit_data($where,'catalog_list')->result();
+
+        $this->load->view('admin/v_header');
+        $this->load->view('admin/v_produk_edit',$data);
+        $this->load->view('admin/v_footer'); 
+    }
+    //untuk menampilkan ke halaman produk 
     function produk_baru(){
         $this->load->view('admin/v_header');
         $this->load->view('admin/v_produk_baru');
         $this->load->view('admin/v_footer');
     }
-//fungsi untuk membuat user admin logout
-    function logout(){
-        $this->session->sess_destroy();
-        redirect(site_url().'c_login?alert=logout');
-    }
-// menampilkan form ganti password user
+    // menampilkan form ganti password user
     function ganti_password(){
         $this->load->view('admin/v_header');
         $this->load->view('admin/v_ganti_password');
         $this->load->view('admin/v_footer');
     }
-// melakukan aksi ganti password user
+    //Untuk edit data petugas user
+    function petugas_edit($id){
+        $where = array('id'=> $id);
+        $data['data_petugas'] = $this->m_data->edit_data($where,'petugas')->result();
+
+        $this->load->view('admin/v_header');
+        $this->load->view('admin/v_petugas_edit',$data);
+        $this->load->view('admin/v_footer'); 
+
+    }
+
+    // untuk testing
+    function testimage(){
+        $this->load->view('admin/v_test_image');
+    }
+
+    // ======================================== END OF VIEW ======================================== \\
+
+
+    // ===================================================== AKSI ===================================================== \\
+    //fungsi untuk membuat user admin logout
+    function logout(){
+        $this->session->sess_destroy();
+        redirect(site_url().'c_login?alert=logout');
+    }
+
+    // upload image
+    public function aksi_upload(){
+        $config['upload_path'] = './gambar/produk_catalog/';
+        $config['allowed_types'] = 'gif|jpg|png';
+    
+        $this->load->library('upload', $config);
+        print_r($this->input->post());
+
+        if ( ! $this->upload->do_upload('gambar')){
+            $error = array('error' => $this->upload->display_errors());
+            // print_r($error);
+            return ""; // return string kosong kalo gagal upload
+        }else{
+            $data = array('upload_data' => $this->upload->data());
+            // print_r($data);
+            return $data['upload_data']['file_name']; // return nama file jika berhasil uplaod
+        }
+    }
+
+    // melakukan aksi ganti password user
     function ganti_password_aksi(){
         $Pbaru = $this->input->post('password_baru');
         $Pulang = $this->input->post('password_ulang');
@@ -66,7 +118,80 @@ class C_admin extends CI_Controller {
             $this->load->view('admin/v_footer');    
         }
     }
-    //CRUD Petugas
+
+    // ----------------------------- CRUD Produk
+    // Tambah Produk (INSERT)
+    function produk_tambah_aksi(){
+        $nama_barang = $this->input->post('nama_barang');
+        $deskripsi = $this->input->post('deskripsi');
+        $tinggi = $this->input->post('tinggi');
+        $lebar = $this->input->post('lebar');
+        $tebal = $this->input->post('tebal');
+        $berat = $this->input->post('berat');
+       
+        // upload gambar
+        $gambar = $this->aksi_upload();
+
+        $data = array(
+            'nama_barang'=>$nama_barang,
+            'deskripsi'=>$deskripsi,
+            'tinggi'=>$tinggi,
+            'lebar'=>$lebar,
+            'tebal'=>$tebal,
+            'berat'=>$berat,
+            'gambar'=>$gambar,
+        );
+
+        $this->m_data->insert_data($data,'catalog_list');
+        redirect(site_url().'c_admin/produk');
+    }
+
+    // Update data produk
+    function produk_update(){
+        $id = $this->input->post('id');
+        $nama_barang = $this->input->post('nama_barang');
+        $deskripsi = $this->input->post('deskripsi');
+        $tinggi = $this->input->post('tinggi');
+        $lebar = $this->input->post('lebar');
+        $tebal = $this->input->post('tebal');
+        $berat = $this->input->post('berat');
+
+        // cek data 
+        $where = array('id'=>$id);
+       
+        // upload gambar jika ada gambar baru
+        if ($_FILES['gambar']['tmp_name'] != "" ) $gambar = $this->input->post('gambarlama');
+        else $gambar = $this->aksi_upload(); // return nama file
+
+        $data = array(
+            'nama_barang'=>$nama_barang,
+            'deskripsi'=>$deskripsi,
+            'tinggi'=>$tinggi,
+            'lebar'=>$lebar,
+            'tebal'=>$tebal,
+            'berat'=>$berat,
+            'gambar'=>$gambar,
+        );
+
+        $result = $this->m_data->update_data($where,$data,'catalog_list');
+        print_r($result);
+        echo "<br>";
+        print_r($this->input->post());
+        // redirect(site_url().'c_admin/produk');
+    }
+
+    // Hapus Produk
+    function produk_hapus($id){
+        $where = array('id'=>$id);
+
+            // menghapus data petugas dari database sesuai id
+        $this->m_data->delete_data($where,'catalog_list');
+
+            // mengalihkan halaman ke halaman data petugas
+        redirect(site_url().'c_admin/produk');
+    }
+
+    // ----------------------------- CRUD Petugas
     function petugas(){
         //mengambil data dari database
         $data['data_petugas'] = $this->m_data->get_data('petugas')->result();
@@ -96,16 +221,6 @@ class C_admin extends CI_Controller {
             //insert ke dalam tabel petugas
         $this->m_data->insert_data($data,'petugas');
         redirect(site_url().'c_admin/petugas');
-    }
-        //Untuk edit data petugas user
-    function petugas_edit($id){
-        $where = array('id'=> $id);
-        $data['data_petugas'] = $this->m_data->edit_data($where,'petugas')->result();
-
-        $this->load->view('admin/v_header');
-        $this->load->view('admin/v_petugas_edit',$data);
-        $this->load->view('admin/v_footer'); 
-
     }
 
         //Update data petugas
@@ -169,5 +284,7 @@ class C_admin extends CI_Controller {
     // }
 
     //***************************************************** */ PRODUCT*********************************************************
+
+    // ===================================================== END OF AKSI ===================================================== \\
     
 }
--- application/views/admin/v_produk_edit.php
+++ application/views/admin/v_produk_edit.php
@@ -1,3 +1,4 @@
+<?php $data_catalog = $data_catalog[0]; ?>
 <div class="container">
 
     <div class="row">
@@ -15,36 +16,38 @@
 
                     <br /><br />
 
-                    <form action="<?php echo site_url() . 'c_admin/produk_tambah_aksi' ?>" method="post" enctype="multipart/form-data">
+                    <form action="<?php echo site_url() . 'c_admin/produk_update' ?>" method="post" enctype="multipart/form-data">
+                        <input type="text" hidden value="<?php echo $data_catalog->id; ?>">
                         <div class="form-group">
                             <label class="font-weight-bold" for="nama_barang">Nama Barang</label>
-                            <input type="text" class="form-control" name="nama_barang" placeholder="Masukan Nama Barang" required="required">
+                            <input type="text" class="form-control" name="nama_barang" placeholder="Masukan Nama Barang" value="<?php echo $data_catalog->nama_barang; ?>" required="required">
                         </div>
                         <div class="form-group">
                             <label class="font-weight-bold" for="deskripsi">Deskripsi</label>
-                            <input type="text" class="form-control" name="deskripsi" placeholder="Masukan Deskripsi Barang" required="required">
+                            <input type="text" class="form-control" name="deskripsi" placeholder="Masukan Deskripsi Barang" value="<?php echo $data_catalog->deskripsi; ?>" required="required">
                         </div>
                         <div class="form-group">
                             <label class="font-weight-bold" for="tinggi">Tinggi</label>
-                            <input type="number" class="form-control" name="tinggi" placeholder="Masukan Tinggi" required="required">
+                            <input type="number" class="form-control" name="tinggi" placeholder="Masukan Tinggi" value="<?php echo $data_catalog->tinggi; ?>" required="required">
                         </div>
                         <div class="form-group">
                             <label class="font-weight-bold" for="lebar">Lebar</label>
-                            <input type="number" class="form-control" name="lebar" placeholder="Masukan Lebar" required="required">
+                            <input type="number" class="form-control" name="lebar" placeholder="Masukan Lebar" value="<?php echo $data_catalog->lebar; ?>" required="required">
                         </div>
                         <div class="form-group">
                             <label class="font-weight-bold" for="tebal">Tebal</label>
-                            <input type="number" class="form-control" name="tebal" placeholder="Masukan Tebal" required="required">
+                            <input type="number" class="form-control" name="tebal" placeholder="Masukan Tebal" value="<?php echo $data_catalog->tebal; ?>" required="required">
                         </div>
                         <div class="form-group">
                             <label class="font-weight-bold" for="berat">Berat</label>
-                            <input type="number" class="form-control" name="berat" placeholder="Masukan Berat" required="required">
+                            <input type="number" class="form-control" name="berat" placeholder="Masukan Berat" value="<?php echo $data_catalog->berat; ?>" required="required">
                         </div>
                         <div class="form-group">
                             <label class="font-weight-bold" for="gambar">Gambar</label>
-                            <input type="file" class="form-control " accept="image/*" id="gambar" name="gambar" required="required">
+                            <input type="file" class="form-control " accept="image/*" id="gambar" name="gambar" value="<?php echo $data_catalog->gambar; ?>">
                         </div>
-                        <img id="showimage" src="#" hidden/>
+                        <img id="showimage" src="<?php echo site_url().'gambar/produk_catalog/'.$data_catalog->gambar; ?>"/>
+                        <input type="text" name="gambarlama" hidden value="<?php echo $data_catalog->gambar; ?>">
                         <br>
                         <input type="submit" class="btn btn-primary" value="Simpan">
                     </form>
